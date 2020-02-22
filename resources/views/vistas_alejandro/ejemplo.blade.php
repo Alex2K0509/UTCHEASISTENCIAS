5b151f3cf5dcf5748f66876e8c6998a5f7b5f64f
Alex2K0509 <alexvergara1998@gmail.com>
2020-02-22 00:04:19 -0500

    en este commit ya se donde van los estilos css, deben ir en la carpeta de vendor/laravel/framework/src/Illuminate/Mail/resources/views/html/themes/default.css

---
+++ resources/views/vistas_alejandro/ejemplo.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app2')
+
+@section('title','Area de maestros')
+
+
+@section('content')
+
+<div class="contenedor1">
+    <div class="form-group">
+        <label>Nombre:</label>
+        <input type="text" class="form-control">
+    </div>
+</div>
+
+
+
+
+    @endsection
+@section('footer')
+
+
+    <footer class="footer1">Hola</footer>
+
+
+@endsection
+
+
