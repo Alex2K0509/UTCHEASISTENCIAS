20c7a0f9c5c45d24f621e2e9c5632f5cf4ed2028
RAFL10 <811811016>
2020-03-05 12:56:39 -0500

    commit1

---
+++ resources/views/includes/sidebarRamiro.blade.php
@@ -0,0 +1,126 @@
+@method('PUT')
+@csrf
+@extends('layouts.layoutRamiro')
+
+
+
+@section('sidebarRamiro')
+
+    <title>Area de Adminww</title>
+    <div class="wrapper">
+
+
+        <!-- Sidebar  -->
+        <nav id="sidebar">
+            <div class="sidebar-header">
+                <h3>Administradores</h3>
+            </div>
+
+            <ul class="list-unstyled components">
+                <p>Bienvenido</p>
+                <li class="active">
+                    <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Opciones</a>
+                    <ul class="collapse list-unstyled" id="homeSubmenu">
+                        <li>
+                            <a  data-toggle="collapse" href="{{ route('HorariosImportados') }}" aria-expanded="false" >Importar Horarios</a>
+
+                        </li>
+                        <li>
+                            <a  data-toggle="collapse" href="{{ route('MateriasImportadas') }}" aria-expanded="false">Importar Materias</a>
+
+                        </li>
+
+                    </ul>
+                </li>
+
+            </ul>
+        </nav>
+
+        <!-- Page Content  -->
+        <div id="content">
+
+            <div class="container-fluid">
+
+                <button type="button" id="sidebarCollapse" class="btn btn-info">
+
+                    <span> <i class="fas fa-bars"></i></span>
+                </button>
+
+            </div>
+
+
+        </div>
+
+
+
+
+        @endsection
+
+        @extends('layouts.layoutRamiro')
+        @section('sidebar')
+             <aside class="main-sidebar sidebar-dark-primary elevation-4" style="background-color: #118740; color: #000000">
+                <!-- Brand Logo -->
+                <a href="{{ route('Principal') }}" class="brand-link">
+                    <i class="fas fa-user-graduate"></i>
+                    <span class="brand-text font-weight-light">Administradores</span>
+                </a>
+
+                <!-- Sidebar -->
+                <div class="sidebar">
+                    <!-- Sidebar user panel (optional) -->
+
+                    <!-- Sidebar Menu -->
+                    <nav class="mt-2">
+                        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+                            <!-- Add icons to the links using the .nav-icon class
+                                 with font-awesome or any other icon font library -->
+                            <li class="nav-item has-treeview menu-open">
+                                <a href="#" class="nav-link ">
+
+                                    <p>
+                                        Opciones
+                                        <i class="right fas fa-angle-left"></i>
+                                    </p>
+                                </a>
+                                <ul class="nav nav-treeview">
+                                    <li class="nav-item">
+                                        <a href="{{ route('HorariosImportados') }}" class="nav-link active">
+
+                                            <p>Importar Horarios</p>
+                                        </a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="{{ route('MateriasImportadas') }}" class="nav-link active">
+
+                                            <p>Importar Materias</p>
+                                        </a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="{{ route('MaestrosImportados') }}" class="nav-link active">
+
+                                            <p>Importar Maestros</p>
+                                        </a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="{{ route('AlumnosImportados') }}" class="nav-link active">
+
+                                            <p>Importar Alumnos</p>
+                                        </a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="#" class="nav-link active">
+
+                                            <p>Cerrar sesión</p>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </li>
+
+                        </ul>
+                    </nav>
+                    <!-- /.sidebar-menu -->
+                </div>
+
+                <!-- /.sidebar -->
+            </aside>
+@endsection()
