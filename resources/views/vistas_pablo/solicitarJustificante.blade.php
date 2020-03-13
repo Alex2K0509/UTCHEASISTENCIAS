2742ab91e0aceed597d75dbbc80b6059b7253653
jpmarin99 <60925818+jpmarin99@users.noreply.github.com>
2020-03-12 22:38:41 -0500

    Update solicitarJustificante.blade.php

--- resources/views/vistas_pablo/solicitarJustificante.blade.php
+++ resources/views/vistas_pablo/solicitarJustificante.blade.php
@@ -8,7 +8,7 @@
         <form action="/Alumno/justificaciones/email">
             <div class="form-group">
                 <label for="materia">Motivo de la falta:</label>
-                <input type="text" class="form-control" id="motivo"  name="motivo">
+                <textarea class="form-control" id="motivo"  name="motivo" required></textarea>
             </div>
             <div class="form-group">
                 <label for="justificante">Justificante:</label>
@@ -17,11 +17,11 @@
             </div>
             <div class="form-group">
                 <label for="date2">Día:</label>
-                <input type="date" class="form-control" id="date2"  name="date2">
+                <input type="date" class="form-control" id="date2"  name="date2"required>
             </div>
             <div class="form-group">
                 <label for="director">Director de carrera:</label>
-                <input type="text" class="form-control" id="director"  name="director" disabled>
+                <input type="text" class="form-control" id="director" placeholder="Martín Antonio Santos Romero"  name="director" disabled>
             </div>
 
 
