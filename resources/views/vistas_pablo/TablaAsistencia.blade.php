0280b6c292347f031dd682fe92587933009637e6
jpmarin99 <60925818+jpmarin99@users.noreply.github.com>
2020-03-16 12:38:45 -0500

    DataTable habilitado

--- resources/views/vistas_pablo/TablaAsistencia.blade.php
+++ resources/views/vistas_pablo/TablaAsistencia.blade.php
@@ -1,68 +1,63 @@
 @extends('layouts.layoutsTablas')
 @section('title','Area de Alumnos')
 @section('content')
+    <div class="card">
+        <div class="card-header">
+            <h3 class="card-title" style="float:top">Nombre del alumno</h3>
 
+        </div>
+        <div class="card-header">
+            <h3 class="card-title" style="text-align: center">Vizualizcion general de asistencias</h3>
 
-    <div class="table-responsive">
-        <table class="table table-bordered">
-            <thead>
-            <tr>
-                <th scope="col" colspan="3" style="text-align: center";>Visualizar asistencias</th>
-            </tr>
-            <tr>
-                <th scope="col" colspan="3" style="text-align: center";>Nombre del alumno</th>
+        </div>
+        <!-- /.card-header -->
+        <div class="card-body">
+            <form action="">
+                <table id="example1" class="table table-bordered table-striped">
+                    <thead>
+                    <tr>
+                        <th>Matricula</th>
+                        <th>Grupo</th>
+                        <th>Materia</th>
+                        <th>Asistencias</th>
+                        <th>Faltas</th>
+                    </tr>
+                    </thead>
+                    <tbody>
 
-            </tr>
+                    <tr>
+                        <td>8118110121</td>
+                        <td>TIAM5</td>
+                        <td>Ingles V</td>
+                        <td>16</td>
+                        <td>0</td>
 
-
-
-            <tr>
-                <th scope="col" colspan="3" style="text-align: center";>Materia</th>
-            </tr>
-
-            <tr>
-                <th scope="col" colspan="3" style="text-align: center";>Rango de fechas seleccionado:.</th>
-            </tr>
-
-
-            </thead>
-
-            <tbody>
-
-            <tr>
-                <th scope="row">Asistencias</th>
-                <th scope="row">Faltas</th>
-                <th scope="row">Porcentaje de asistencia</th>
-
-            </tr>
-
-
-            <tr>
-                <th scope="row">
-                    <p>20</p>
-                </th>
-                <td>
-                    <p>4</p>
-                </td>
-                <td>
-                    80%
-                </td>
-            </tr>
-            <tr>
+                    </tr>
+                    <tr>
+                        <td>8118110121</td>
+                        <td>TIAM5</td>
+                        <td>IOT</td>
+                        <td>30</td>
+                        <td>2</td>
+                    </tr>
+                    <tr>
+                     <td>8118110121</td>
+                        <td>TIAM5</td>
+                        <td>EOYE II</td>
+                        <td>18</td>
+                        <td>0</td>
 
 
 
+                    </tr>
 
-            </tr>
+                    </tbody>
 
-
-            </tbody>
-        </table>
-        </form>
+                </table>
+            </form>
+        </div>
+        <!-- /.card-body -->
     </div>
-
-
-
 
 
 @endsection
--- resources/views/vistas_pablo/vizualizarAsistencia.blade.php
+++ resources/views/vistas_pablo/vizualizarAsistencia.blade.php
@@ -6,26 +6,14 @@
     <div class="container">
         <h2>Visualizar asistencia</h2>
             <div class="form-group">
-                <label for="materia">Materia/Grupo:</label>
-                <select id="materias" class="custom-select">
-                    <option>Inglés V Grupo 5 TIAM18</option>
-                    <option>EOYE II Grupo 5 TIAM18</option>
-                    <option>IOT Grupo 5 TIAM18</option>
-                    <option>Desarrollo movil Grupo 5 TIAM18  </option>
-                    <option>Todas</option>
-
-
-                </select>
-            </div>
-            <div class="form-group">
                 <label for="date3">Fecha Inicial</label>
-                <input type="date" class="form-control" id="date3"  name="date3">
+                <input type="date" class="form-control" id="date3"  name="date3" required>
             </div>
 
 
         <div class="form-group">
             <label for="datea">Fecha Final</label>
-            <input type="date" class="form-control" id="date4"  name="date4">
+            <input type="date" class="form-control" id="date4"  name="date4" required>
         </div>
 
         <input type="submit" class="btn-outline-success" style="float: right" value="Solicitar asistencias">
