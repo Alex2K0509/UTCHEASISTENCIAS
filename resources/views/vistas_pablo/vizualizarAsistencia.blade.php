1bba5c5f62eb82d49341141543f599441e53fd54
jpmarin99 <60925818+jpmarin99@users.noreply.github.com>
2020-04-02 06:53:17 -0500

    base de datos

--- resources/views/vistas_pablo/vizualizarAsistencia.blade.php
+++ resources/views/vistas_pablo/vizualizarAsistencia.blade.php
@@ -10,20 +10,61 @@
         <div class="container">
             <h2>Visualizar asistencia</h2>
             <div class="form-group">
-                <label for="date3">Fecha Inicial</label>
-                <input type="date" class="form-control" id="fecha1"  name="fecha1" required>
+                <label for="materia">Materia:</label>
+                <select id="materia" class="custom-select" name="materia">
+                    {{---el if es para ver si hay una relacion entree usuarios y generica, puede ser coun-exist---}}
+                    @if ($user->genericas()->count() > 0)
+                        {{---en este primer forech nos da sus genericas ---}}
+                        @foreach ($user->genericas as $generica)
+                            {{---en este segundo foreach nos esta dando las asignaturas que tiene ---}}
+                            @foreach ($generica->Materias as $Materias)
+
+                                <option value="{{$Materias->Id_Asignatura}}">{{$Materias->nombre_materia}}</option>
+                            @endforeach
+
+                        @endforeach
+                    @endif
+
+                </select>
             </div>
 
 
             <div class="form-group">
-                <label for="datea">Fecha Final</label>
-                <input type="date" class="form-control" id="fecha2"  name="fecha2" required>
+                <label for="grupo">Grupo:</label>
+                <select id="grupo" class="custom-select" name="grupo" disabled>
+                    {{---el if es para ver si hay una relacion entree usuarios y generica, puede ser coun-exist---}}
+                    @if ($user->genericas()->count() > 0)
+                        {{---en este primer forech nos da sus genericas ---}}
+                        @foreach ($user->Genericas as $genericas)
+                            {{---en este segundo foreach nos esta dando las asignaturas que tiene ---}}
+                            <option value="{{$genericas->Id_grupo}}">
+                                {{$genericas->Id_grupo}}
+                            </option>
+
+
+                        @endforeach
+                    @endif
+
+                </select>
             </div>
 
-            <input type="submit" class="btn-outline-success" style="float: right" value="Solicitar asistencias">
 
+            <div class="form-group">
+                <label for="date1">Fecha inicial:</label>
+                <input type="date" class="form-control" id="date1"  name="date1">
+            </div>
+
+
+            <div class="form-group">
+                <label for="date2">Fecha final:</label>
+                <input type="date" class="form-control" id="date2"  name="date2">
+            </div>
+
+
+            <button type="submit"  class="btn-outline-success">Solicitar asistencia</button>
         </div>
     </form>
+
 
 @endsection
 @section('sidebar')
